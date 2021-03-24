f2c39496ad3dad9983995a450807d682c00d3cff
P-hansen <philip.pt.hansen@gmail.com>
2021-03-24 20:42:48 +0000

    renderTweets function working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,21 +28,36 @@ $(document).ready(function() {
     };
 
     // Test / driver code (temporary). Eventually will get this from the server.
-    const tweetData = {
-        "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
+    const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
             "handle": "@SirIsaac"
-        },
-        "content": {
+          },
+          "content": {
             "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
         },
-        "created_at": 1461116232227
-    }
-
-    const $tweet = createTweetElement(tweetData);
-
-    // Test / driver code (temporary)
-    console.log("here is the final tweet:", $tweet); // to see what it looks like
-    $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
+        }
+      ]
+      
+      const renderTweets = function(tweets) {
+        for (const chunks in tweets) {
+          $('#tweet-container').append(createTweetElement(tweets[chunks]));
+        }
+      }
+      
+      renderTweets(data);
 });
