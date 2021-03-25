2fd70db956afa541842fc2aa0c52ab1d2aab7815
P-hansen <philip.pt.hansen@gmail.com>
2021-03-25 23:18:23 +0000

    resize for desktop

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,6 +16,19 @@ $(document).ready(function() {
     }
     getTweetsFromServer();
 
+    //clicking the write a new tweet slides the compose section down
+    let downFlag = false;
+    $(".right").on("click", function(event){
+        if (downFlag === false) {
+            $(".new-tweet").slideDown();
+            $("#tweet-text").focus();
+            downFlag = true;
+        } else {
+            $(".new-tweet").slideUp();
+            downFlag = false;
+        }
+    });
+
     //on form submit shove that tweet into the server
     $("form").on("submit", function(event) {
         event.preventDefault();
